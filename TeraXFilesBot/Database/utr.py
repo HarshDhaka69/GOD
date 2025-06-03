6d195a9ac8fa12efb17222ea45c3bbbd13f925ce
HarshDhaka69 <149968506+HarshDhaka69@users.noreply.github.com>
2025-06-04 04:40:13 +0530

    Delete TeraXFilesBot directory

--- TeraXFilesBot/Database/utr.py
+++
@@ -1,9 +0,0 @@
-from . import db
-
-db = db.utr
-
-async def add_utr(utr):
-    await db.insert_one({"utr": utr})
-
-async def is_utr_used(utr):
-    return await db.find_one({"utr": utr})
--- TeraXFilesBot/Plugins/utils.py
+++
@@ -1,7 +0,0 @@
-def format_size(size, decimal_places=2):
-    units = ["B", "KB", "MB", "GB", "TB", "PB"]
-    for unit in units:
-        if size < 1024:
-            return f"{size:.{decimal_places}f} {unit}"
-        size /= 1024
-    return f"{size:.{decimal_places}f} PB"
